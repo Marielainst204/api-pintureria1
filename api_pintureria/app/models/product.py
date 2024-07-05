4a23bc5d1693c4316cf743c2f8ba778508d2edee
Mariela <167578715+Marielainst204@users.noreply.github.com>
2024-07-05 17:47:11 -0300

    primer commit

---
+++ api_pintureria/app/models/product.py
@@ -0,0 +1,129 @@
+from app.database import get_db
+#from app.models.review import Review
+
+class Producto:
+    # Constructor de la clase Product
+    def __init__(self, id_producto=None, nombre=None, precio=None, poster_url=None, reviews=[]):
+        self.id_producto = id_producto  # ID del producto, se asigna automáticamente para nuevas películas
+        self.nombre = nombre  # Nombre del producto
+        self.precio = precio  # Precio del producto
+        self.poster_url = poster_url  # URL del póster del producto
+        self.reviews = reviews
+
+        # self.reviews = reviews if reviews is not None else []  # Lista de reseñas asociadas a la película
+
+    # Método para guardar o actualizar un producto en la base de datos
+    def save(self):
+        db = get_db()  # Obtener la conexión a la base de datos
+        cursor = db.cursor()
+        if self.id_producto:
+            # Si el producto ya tiene un ID, se actualiza su registro en la base de datos
+            cursor.execute("""
+                UPDATE productos SET nombre = %s, precio = %s, poster_url = %s
+                WHERE id_producto = %s
+            """, (self.nombre,self.precio, self.poster_url, self.id_producto))
+        else:
+            # Si el producto no tiene un ID, se inserta un nuevo registro en la base de datos
+            cursor.execute("""
+                INSERT INTO productos (nombre, precio, poster_url) VALUES (%s, %s, %s)
+            """, (self.name,self.price,self.poster_url))
+            self.id_producto = cursor.lastrowid  # Obtener el ID asignado por la base de datos
+        db.commit()  # Confirmar la transacción
+        cursor.close()
+
+    # Método estático para obtener todas los productos con sus reseñas de la base de datos
+    @staticmethod
+    def get_all():
+        """
+        Retorna un listado de OBJETOS Producto, cada uno con sus reseñas.
+        """
+        db = get_db()  # Obtener la conexión a la base de datos
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT 
+                p.id_producto, p.nombre, p.precio, m.poster_url,
+               # r.id_review, r.reviewer_name, r.comment, 
+            FROM 
+               Productos p
+            LEFT JOIN 
+                reviews r ON p.id_producto = r.id_producto
+        """)  # Ejecutar la consulta para obtener todaslos productos con sus reseñas
+        rows = cursor.fetchall()  # Obtener todos los resultados
+        
+        products_dict = {}
+        
+        for row in rows:
+            id_producto = row[0]
+            if id_producto not in productos_dict:
+                productos_dict[id_producto] = Producto(
+                    id_producto=row[0], nombre=row[1], precio=row[2], poster_url=row[3],  reviews=[]
+                )
+             if row[5] is not None:
+                 review = Review(
+                    id_review=row[5], id_product=row[0], reviewer_name=row[6], comment=row[7], #DUDA SI VA ESO
+                )
+                productos_dict[id_producto].reviews.append(review)
+
+        cursor.close()
+        return list(products_dict.values())  # Devolver la lista de PRODUCTOS con sus reseñas
+
+    @staticmethod
+    def get_by_id(producto_id):
+        db = get_db()
+        cursor = db.cursor()
+
+        # Ejecutar la consulta con JOIN para obtenerel producto y sus reseñas
+        #CREO QUE NO ES NECESARIO
+        cursor.execute("""
+            SELECT   
+                p.id_producto, p.nombre, p.precio, p.poster_url,
+                r.id_review, r.reviewer_name, r.comment, r.rating
+            FROM 
+               productos p
+            LEFT JOIN 
+                reviews r ON m.id_producto = r.id_producto
+            WHERE 
+                p.id_producto = %s
+        """, (producto_id,))
+        
+        rows = cursor.fetchall()
+        cursor.close()
+
+        if rows:
+            # Utilizamos un diccionario para mapear los productos por su ID para evitar duplicados
+            producto_map = {}
+            for row in rows:
+                if row[0] not in producto_map:
+                    # Si el producto aún no está en el mapeo, la añadimos con sus datos básicos
+                    producto_map[row[0]] = Producto(id_producto=row[0],nombre=row[1],precio=row[2], poster_url=row[3],reviews=[])
+
+                # Añadir la reseña si existe (puede ser None si no hay reseñas asociadas)
+                #NO LO VEO NECESARIO
+                #if row[5] is not None:
+                    #review = Review(id_review=row[5], id_movie=row[0], reviewer_name=row[6], comment=row[7], rating=row[8])
+                    #movie_map[row[0]].reviews.append(review)
+
+            # Devolver EL PRODUCTO encontrada por su ID
+            return producto_map[producto_id]
+
+        return None  # Si no se encontró el producto, devolver None
+    # Método para eliminar un producto de la base de datos
+    def delete(self):
+        db = get_db()  # Obtener la conexión a la base de datos
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM productos WHERE id_producto = %s", (self.id_producto,))  # Ejecutar la consulta para eliminar el producto
+        db.commit()  # Confirmar la transacción
+        cursor.close()
+
+    # Método para serializar un objeto Product a un diccionario
+    def serialize(self):
+        return {
+            'id_producto': self.id_producto,  # ID del producto
+            'nombre': self.nombre,  # Nombre del producto
+            'precio': self.precio,  # Precio del producto
+            'poster_url': self.poster_url,  # URL del póster del producto
+            #'reviews': [review.serialize() for review in self.reviews]  # Lista de reseñas serializadas
+        }
+
+    def __str__(self):
+        return f"PRODUCTO: {self.id_producto} - {self.nombre} - {self.precio}"
---
+++ api_pintureria/inicial.py
@@ -0,0 +1,122 @@
+import os, time
+# Instalar con pip install flask
+from flask import Flask, jsonify, request
+# Instalar con pip install flask-cors
+from flask_cors import CORS
+
+"""
+Para trabajar con archivos asegurar que un nombre de archivo 
+proporcionado por el usuario sea seguro para guardarlo en el sistema de archivos.
+"""
+# Si es necesario, pip install Werkzeug 
+from werkzeug.utils import secure_filename
+
+from app.models.product import Product
+from app.database import init_app, init_db
+
+"""
+Crear las dependencias
+pip freeze > requirements.txt
+
+Instalar todas las dependencias
+pip install -r requirements.txt
+
+"""
+d = os.path.dirname(__file__)
+os.chdir(d)
+
+ruta_destino = 'static/img/'
+
+
+app = Flask(__name__)
+CORS(app)
+
+# Inicializar la base de datos con la aplicación Flask
+init_app(app)
+
+@app.route('/init-db')
+def init_db_route():
+    init_db()
+    return "Base de datos inicializada correctamente."
+
+
+
+@app.route('/products', methods=['POST'])
+def create_product():
+    print("HOLA")
+    # data = request.json
+    data = request.form
+    print("que hay en data ", data)
+    archivo=request.files['imagen']
+    print(archivo)
+    # Trabajamos con la imagen
+    # Utilizamos la función `secure_filename` para obtener un nombre de archivo seguro para la imagen cargada. 
+    # Esta función elimina caracteres especiales que podrían causar problemas de seguridad.
+    nombre_imagen = secure_filename(archivo.filename)
+
+    print(">>>>>>>>>", nombre_imagen)
+
+    # Separamos el nombre base del archivo y su extensión utilizando `os.path.splitext`.
+    # Esto nos permite trabajar con el nombre y la extensión por separado.
+    nombre_base, extension = os.path.splitext(nombre_imagen)
+
+    # Generamos un nuevo nombre para el archivo utilizando el nombre base original y 
+    # agregando un timestamp para asegurarnos de que el nombre del archivo sea único.
+    # Concatenamos el nombre base, un guion bajo, el timestamp actual y la extensión.
+    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
+
+    # Guardamos el archivo en la ruta de destino utilizando el nuevo nombre generado.
+    # `os.path.join` se asegura de que la ruta sea correcta, sin importar el sistema operativo.
+    archivo.save(os.path.join(ruta_destino, nombre_imagen))
+
+
+
+    new_producto = Producto(nombre=data['nombre'], precio=data['precio'],  poster_url=nombre_imagen)
+    new_producto.save()
+    return jsonify({'message': 'Producto creado correctamente 😎'}), 201
+
+
+@app.route('/producto', methods=['GET'])
+def get_all_producto(): #FIJARSE SI ESTA BIEN
+    producto = Producto.get_all()
+    producto_json=[]
+    for p in producto:
+         producto_json.append(p.serialize())
+    return  producto_json
+    # Manera resumida:
+    # return jsonify([movie.serialize() for movie in movies])
+
+
+@app.route('/producto/<int:id>', methods=['GET'])
+def get_by_id_producto(id):
+    product = Producto.get_by_id(id)
+    if producto:
+        return jsonify(producto.serialize())
+    else:
+        return jsonify({'message': 'Producto no encontrado'}), 404
+
+@app.route('/producto/<int:id>', methods=['DELETE'])
+def delete_producto(id):
+    producto = Producto.get_by_id(id)
+    if not producto:
+        return jsonify({'message': 'Producto no encontrado'}), 404
+    producto.delete()
+    return jsonify({'message': 'El producto fue borrado'})
+
+@app.route('/producto/<int:id>', methods=['PUT'])
+def update_producto(id):
+    producto = Producto.get_by_id(id)
+    if not producto:
+        return jsonify({'message': 'Producto no encontrado'}), 404
+    data = request.form
+    producto.nombre = data.get('nombre', producto.nombre)
+    producto.precio = data.get('precio', producto.precio)
+   
+    # producto.poster_url = data.get('poster_url', producto.poster_url)
+    producto.save()
+    return jsonify({'message': 'Producto actualizado correctamente 😋'})
+
+
+if __name__ == '__main__':
+     app.run(debug=True)
+     #app.run(host='0.0.0.0', port=5000) #ver el puerto
