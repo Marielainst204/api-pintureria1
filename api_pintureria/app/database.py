358225fd717d9d34a24f99e5deaa53b0cf032cbe
Mariano <mk1110aballay@gmail.com>
2024-07-07 22:46:31 -0300

    modificar archivos

--- api_pintureria/app/database.py
+++ api_pintureria/app/database.py
@@ -1,6 +1,6 @@
 import os 
 # pip install mysql-connector-python
-import mysql.connector  # Importa el conector MySQL para conectar con la base de datos
+import mysql.connector # Importa el conector MySQL para conectar con la base de datos
 from flask import g  # Importa g de Flask para almacenar datos durante la petición
 # pip install python-dotenv
 from dotenv import load_dotenv  
@@ -17,19 +17,18 @@ DATABASE_CONFIG = {
     'password': os.getenv('DB_PASSWORD'),  
     'host': os.getenv('DB_HOST'),  
     'database': os.getenv('DB_NAME'),  
-    'port': os.getenv('DB_PORT', 3306)  # Puerto del servidor de la base de datos, por defecto es 3306 si no se especifica
+    'port': os.getenv('DB_PORT', 3306) ,
+     # Puerto del servidor de la base de datos, por defecto es 3306 si no se especifica
 }
 
 
 # Función para obtener la conexión de la base de datos
 def get_db():
-    # Si no hay una conexión a la base de datos en g, la creamos
-    # g, que es un objeto de Flask que se usa para almacenar datos durante la vida útil de una solicitud.
-    if 'db' not in g:
-        print("···· Abriendo conexion a DB ····",DATABASE_CONFIG['database']," ---- ",DATABASE_CONFIG['user'])
-        g.db = mysql.connector.connect(**DATABASE_CONFIG)
-    # Retorna la conexión a la base de datos
+   if 'db' not in g:
+    g.db = mysql.connector.connect(**DATABASE_CONFIG)
+  
     return g.db
+    
 
 # Función para cerrar la conexión a la base de datos
 def close_db(e=None):
